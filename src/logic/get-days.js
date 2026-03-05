6e40c935df6a5fb6859e1ddfe908ddc5f5789ce6
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-03-05 13:13:09 -0800

    Refactor day getter to be work with any timezone

--- src/logic/get-days.js
+++ src/logic/get-days.js
@@ -1,18 +1,13 @@
-import { data } from "../index.js";
-
-const MILLISECONDS_IN_SECOND = 1000;
-const MILLISECONDS_IN_HOUR = 1000 * 60 * 60;
-
 // Returns day of the week in selected timezone
-const getDay = (datetimeEpoch) => {
-  const adjustedTime =
-    datetimeEpoch * MILLISECONDS_IN_SECOND +
-    data.tzoffset * MILLISECONDS_IN_HOUR;
-  const date = new Date(adjustedTime);
-  return date.toLocaleString("en-US", { weekday: "long", timeZone: "UTC" });
+const getDay = (dayData, data) => {
+  const date = new Date(dayData.datetime);
+  return date.toLocaleString("en-US", {
+    weekday: "long",
+    timeZone: `${data.timezone ?? "UTC"}`,
+  });
 };
 
 // Returns 7 days starting from current day
-const getDays = () => data.days.map((day) => getDay(day.datetimeEpoch));
+const getDays = (data) => data.days.map((day) => getDay(day, data));
 
 export { getDays };
