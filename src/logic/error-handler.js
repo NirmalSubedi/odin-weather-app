8c5adb3c432f320bc2190e5c2012927e8125b3d6
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-03-08 18:37:55 -0700

    Change error message displayed on invalid location
    Provide actionable and user friendly error message

--- src/logic/error-handler.js
+++ src/logic/error-handler.js
@@ -4,8 +4,12 @@ const handleError =
   (fn) =>
   (...params) =>
     fn(...params).catch((error) => {
-      showError(error);
       console.error(error);
+      if (error.message === "Request did not match an endpoint.") {
+        error.name = "Location not found";
+        error.message = "Please enter a valid location.";
+      }
+      showError(error);
     });
 
 export { handleError };
