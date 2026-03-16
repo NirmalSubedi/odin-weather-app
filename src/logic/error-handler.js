44603d86b20dbaa8a15df6e5d7c7619c2a4d8fc7
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-03-16 16:12:31 -0700

    Remove modification of error message in if check
    The condition doesn't cover all cases of incorrect location input
    and the original message will suffice in providing feedback.

--- src/logic/error-handler.js
+++ src/logic/error-handler.js
@@ -5,10 +5,6 @@ const handleError =
   (...params) =>
     fn(...params).catch((error) => {
       console.error(error);
-      if (error.message === "Request did not match an endpoint.") {
-        error.name = "Location not found";
-        error.message = `Location not found. Try searching for: London, New York, Tokyo, Miami, or Paris.`;
-      }
       showError(error);
     });
 
