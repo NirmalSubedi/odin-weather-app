b79f593e9393521a1a842e216be2b9774bf749ed
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-03-16 20:28:41 -0700

    Update display-info function
    Call display alert and temp functions for ui change.
    
    Miscellaneous:
    - show intro screen by default and hide it after searching
    - remove placeholder text in weather section
    - add column gap to secondary-info section to space out children
    when stacked on smaller screen
    - updateQuery data when there is defined

--- src/ui/display-info.js
+++ src/ui/display-info.js
@@ -2,16 +2,20 @@ import {
   requestWeatherInfo,
   getLatestHourData,
   getCardinalDirection,
+  displayTemp,
+  displayAlert,
+  updateQueryData,
 } from "../index.js";
 
-const [primaryInfoContainer, secondaryInfoContainer] =
-  document.body.querySelectorAll(
-    ".weather .primary-info, .weather .secondary-info"
-  );
-const [location, temperature, description, minimumTemp, maximumTemp] =
-  primaryInfoContainer.querySelectorAll(
-    ".location, .temperature, .description, .min-temp, .max-temp"
-  );
+const [introduction, weather] = document.body.querySelectorAll(
+  ".introduction, .weather"
+);
+const [primaryInfoContainer, secondaryInfoContainer] = weather.querySelectorAll(
+  ".primary-info, .secondary-info"
+);
+const [location, description] = primaryInfoContainer.querySelectorAll(
+  ".location, .description"
+);
 const [windInfo, visibilityInfo, precipitationInfo] =
   secondaryInfoContainer.querySelectorAll(
     ".wind-section .info, .visibility-section .info, .precipitation-section .info"
@@ -20,18 +24,19 @@ const [windInfo, visibilityInfo, precipitationInfo] =
 const displayInformation = async () => {
   const data = await requestWeatherInfo();
   if (!data) return;
+  updateQueryData(data);
 
   const currentHour = getLatestHourData(data);
-  const { resolvedAddress, currentDay } = data;
-  const { tempmin, tempmax } = currentDay;
-  const { temp, conditions, windspeed, winddir, visibility, precipprob } =
+  const { resolvedAddress } = data;
+  const { conditions, windspeed, winddir, visibility, precipprob } =
     currentHour;
 
+  introduction.classList.add("hidden");
+  weather.classList.remove("hidden");
+  displayAlert();
+  displayTemp();
   location.textContent = resolvedAddress;
-  temperature.textContent = `${Math.floor(temp)}°F`;
   description.textContent = conditions;
-  minimumTemp.textContent = `Min: ${Math.floor(tempmin)}°F`;
-  maximumTemp.textContent = `Max: ${Math.floor(tempmax)}°F`;
   windInfo.textContent = `${windspeed} mph ${getCardinalDirection(winddir)}`;
   visibilityInfo.textContent = `${visibility} mi`;
   precipitationInfo.textContent = `${precipprob}%`;
